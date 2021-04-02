506391ed2006420be487cacca5e3d9fed2b3a6ad
xiangliang <xiang_liang@outlook.com>
2021-04-02 17:44:52 +0800

    构建查看JDK8源码的工程

---
+++ src/com/xxll/test/hashmap/Test.java
@@ -0,0 +1,23 @@
+package com.xxll.test.hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author xxll
+ * @date 2021年04月02日16:36
+ * @package com.xxll.test.hashmap
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+        Map<String,Double> hashMap = new HashMap<>();
+        hashMap.put("k1",0.1);
+        hashMap.put("k2",0.2);
+        hashMap.put("k3",0.3);
+        hashMap.put("k4",0.4);
+
+    }
+
+}
